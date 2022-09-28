35f7705aca15a2b6b8c1e4589c0c1c74089c811b
Amber5kye <114544163+Amber5kye@users.noreply.github.com>
2022-09-28 11:59:13 +1000

    Started Week 2 Activities

---
+++ Assets/Scripts/WeekTwoActivities.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AmberBell
+{
+    public class WeekTwoActivities : MonoBehaviour
+    {
+        private string favouriteGame = "Minecraft";
+        private int hoursPlayed = 500;
+        private float costOfGame = 60f;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("My favourite game is " + favouriteGame + "!");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
